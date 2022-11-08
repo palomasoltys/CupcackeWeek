a70d34d4f95c45c809f1add0d98d7e90aa0e551a
palomasoltys <pfs601@gmail.com>
2022-11-08 14:32:56 -0500

    Create while loop to manage the ordering variable and add the items to the order array

--- src/Order.java
+++ src/Order.java
@@ -8,7 +8,7 @@ public class Order {
         Scanner scan = new Scanner(System.in);
         System.out.println("Hello customer. Would you like to place an order? (Y or N)");
         String placeOrder = scan.nextLine();
-        ArrayList<Object> order = new ArrayList<>();
+        ArrayList<Object> order = new ArrayList<Object>();
         if(placeOrder.equalsIgnoreCase("Y")) {
             order.add(LocalDate.now());
             order.add(LocalTime.now());
@@ -25,7 +25,27 @@ public class Order {
                 System.out.println(itemNumber+". "+drink.getName()+ " - "+drink.getPrice()+".");
                 itemNumber++;
             }
-
+            boolean ordering = true;
+            while(ordering) {
+                System.out.println("What would you like to order? Please use the number associated with each item to order.");
+                int orderChoice = scan.nextInt();
+                scan.nextLine();
+                switch (orderChoice) {
+                    case 1 -> order.add(cupcakeMenu.get(1));
+                    case 2 -> order.add(cupcakeMenu.get(2));
+                    case 3 -> order.add(cupcakeMenu.get(3));
+                    case 4 -> order.add(drinkMenu.get(1));
+                    case 5 -> order.add(drinkMenu.get(2));
+                    case 6 -> order.add(drinkMenu.get(3));
+                    default -> System.out.println("Sorry, we don't have this item on the menu.");
+                }
+                System.out.println("Item added to order");
+                System.out.println("Would you like to continue ordering? ");
+                placeOrder = scan.nextLine();
+                if(!placeOrder.equalsIgnoreCase("Y")) {
+                    ordering = false;
+                }
+            }
         } else {
             System.out.println("Have a nice day then.");
         }
